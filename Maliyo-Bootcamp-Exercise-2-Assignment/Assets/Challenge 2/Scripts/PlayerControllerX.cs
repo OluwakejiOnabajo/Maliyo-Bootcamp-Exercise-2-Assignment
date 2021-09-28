0c10868cff5bd46c4d791a3a93ca7b2694fd8281
Onabajo Oluwakeji Daniel <62175369+OnabajoOluwakeji@users.noreply.github.com>
2021-09-28 04:27:17 +0100

    Assignment commpleted

---
+++ Maliyo-Bootcamp-Exercise-2-Assignment/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireRate = 1.1f;
+    private float nextFire = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if enough time has passed since last fire time send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            //Add current time to fire rate
+            nextFire = Time.time + fireRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
